a241b803bbe1678f21551f4dfa556081aadecbc2
IVANA <K.yohivana@awdpay.com>
2026-03-02 20:40:21 +0100

    test merge sprint1 merge yvan

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,12 +1,5 @@
-'use client'
-
 import { DashboardShell } from '@/components/dashboard/shared/DashboardShell'
-import { PresenceProvider } from '@/lib/presence/presence-context'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <PresenceProvider>
-      <DashboardShell role="admin">{children}</DashboardShell>
-    </PresenceProvider>
-  )
+  return <DashboardShell role="admin">{children}</DashboardShell>
 }
